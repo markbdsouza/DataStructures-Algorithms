cd53bb2d047f7c59bc2f9612a97bee3e16e85471
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-09-08 00:04:50 +0530

    array chunks

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   let finalChunk = [];
+//   let miniChunk = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (miniChunk.length === size) {
+//       finalChunk.push(miniChunk);
+//       miniChunk = [];
+//     }
+
+//     miniChunk.push(array[i]);
+//     if (i === array.length - 1) {
+//       finalChunk.push(miniChunk);
+//     }
+//   }
+//   return finalChunk;
+// }
+
+function chunk(array, size) {
+  let chunk = [];
+
+  for (let i = 0; i < array.length / size; i++) {
+    chunk.push(array.slice(i * size, (i + 1) * size));
+  }
+
+  return chunk;
+}
+console.log(chunk([1, 2, 3, 4, 5], 3));
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7], 2));
 
 module.exports = chunk;
